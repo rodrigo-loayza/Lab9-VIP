a1b3c111fc79326b58feddb9198358c3b7528f5a
olive <oliver.bustamante@pucp.edu.pe>
2021-11-22 20:01:08 -0500

    Base dao

---
+++ src/main/java/pe/edu/pucp/vip/Dao/BaseDAO.java
@@ -0,0 +1,22 @@
+package pe.edu.pucp.vip.Dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class BaseDAO {
+
+    public Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        final String user = "root";
+        final String pass = "root";
+        final String url = "jdbc:mysql://localhost:3306/lab9";
+
+        return DriverManager.getConnection(url, user, pass);
+    }
+}
